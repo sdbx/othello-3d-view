42b81259b11f2cc1d4a851ad95eac069eafcc672
Duknam Yoo <yoo2001818@gmail.com>
2018-03-01 21:13:32 +0900

    Implement normal shader test code

--- client-test/index.js
+++ client-test/index.js
@@ -2,8 +2,9 @@ import Renderer from 'webglue/lib/renderer';
 import Camera from 'webglue/lib/camera';
 import CameraController from 'webglue/lib/contrib/blenderController';
 
-import Transform from 'webglue/lib/transform';
+import MeshTransform from 'webglue/lib/meshTransform';
 import box from 'webglue/lib/geom/box';
+import calcNormals from 'webglue/lib/geom/calcNormals';
 
 document.body.style.margin = '0';
 document.body.style.padding = '0';
@@ -23,28 +24,38 @@ let gl = canvas.getContext('webgl', { antialias: true }) ||
 let renderer = new Renderer(gl);
 
 let prevTime = -1;
-let timer = 0;
 
 // Create controller
 let camera = new Camera();
 let controller = new CameraController(canvas, document, camera);
 
-let transform = new Transform();
+let transform = new MeshTransform();
 transform.rotateY(Math.PI / 4);
-let geometry = renderer.geometries.create(box());
+let geometry = renderer.geometries.create(calcNormals(box()));
 let shader = renderer.shaders.create(`
+#version 100
+
 attribute vec3 aPosition;
+attribute vec3 aNormal;
 
 uniform mat4 uProjection;
 uniform mat4 uView;
 uniform mat4 uModel;
+uniform mat3 uNormal;
 
-void main() {
+varying lowp vec3 vColor;
+
+void main(void) {
   gl_Position = uProjection * uView * uModel * vec4(aPosition, 1.0);
+  lowp vec3 normalDir = normalize(
+    (uView * vec4(uNormal * aNormal, 0.0)).xyz);
+  vColor = normalDir * 0.5 + vec3(0.5, 0.5, 0.5);
 }
 `, `
+varying lowp vec3 vColor;
+
 void main() {
-  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  gl_FragColor = vec4(vColor, 1.0);
 }
 `);
 
@@ -52,18 +63,22 @@ function animate(time) {
   if (prevTime === -1) prevTime = time;
   let delta = (time - prevTime) / 1000;
   prevTime = time;
-  timer += delta;
 
+  transform.rotateY(delta * 3);
   controller.update(delta);
 
   renderer.render({
     options: {
       clearColor: new Float32Array([0, 0, 0, 1]),
+      clearDepth: 1,
+      cull: gl.BACK,
+      depth: gl.LEQUAL,
     },
     geometry: geometry,
     shader: shader,
     uniforms: {
       uModel: transform.get,
+      uNormal: transform.getNormal,
       uView: camera.getView,
       uProjection: camera.getProjection,
     },
